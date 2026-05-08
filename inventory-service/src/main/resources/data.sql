92cfa789fbd76ddcca78fa82c920c522d05014ad
omarbaasharalt <omar.baashar@gmail.com>
2026-05-08 12:25:41 +0300

    Added HTML with CSS Design, Fixed API-Gateway, Added data.sql

---
+++ inventory-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO product (name, quantity, location) VALUES ('MacBook Pro', 50, 'California');
+INSERT INTO product (name, quantity, location) VALUES ('Dell XPS', 2, 'Texas');
+INSERT INTO product (name, quantity, location) VALUES ('Lenovo ThinkPad', 15, 'New York');
+
